d45fbc38d651e4c38f64c95a6c00c38fa1bed526
QuesadillaBomb <noahjmaddox@charter.net>
2021-02-22 13:54:52 -0500

    Starting enemy AI code

---
+++ Assets/_Scripts/EnemyAI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    float followRadius;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool distanceFromPlayer(float playerPosition, float enemyPosition)
+    {
+        if (Mathf.Abs(playerPosition - enemyPosition) < followRadius)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool distanceForAttack(float playerPosition, float enemyPosition)
+    {
+        if (Mathf.Abs(playerPosition - enemyPosition) < followRadius)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ Assets/_Scripts/EnemyMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : EnemyAI
+{
+    [SerializeField] Transform playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (distanceFromPlayer(playerTransform.position.x, transform.position.x))
+        {
+            if (playerTransform.position.x < transform.position.x)
+            {
+                Debug.Log("Hi");
+            }
+            else
+            {
+                
+            }
+        }
+    }
+}
