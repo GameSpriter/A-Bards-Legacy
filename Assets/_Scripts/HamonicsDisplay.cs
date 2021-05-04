72594158475a9e3a7093adeca06d3cf5c66d3f8a
Game Spriter <GameSpriter589@gmail.com>
2021-05-04 16:25:07 -0400

    Updated Harmonics Display to have it follow the camera

--- Assets/_Scripts/HamonicsDisplay.cs
+++ Assets/_Scripts/HamonicsDisplay.cs
@@ -57,7 +57,7 @@ public class HamonicsDisplay : MonoBehaviour
             lr.loop = true;
         }
 
-        musicSheetPos.transform.position = new Vector3(musicSheetPos.transform.position.x, 7.0f);
+        musicSheetPos.transform.position = new Vector3(musicSheetPos.transform.position.x, Camera.main.transform.position.y + 7.0f);
         //noteTrackScript = new NoteTracker();
         noteSpawnerScript = musicSheetPos.GetComponent<NoteSpawn>();
 
@@ -91,7 +91,7 @@ public class HamonicsDisplay : MonoBehaviour
         {
             musicSheetPos.SetActive(true);
             musicSheetPos.transform.position = Vector3.MoveTowards(musicSheetPos.transform.position,
-                    new Vector3(musicSheetPos.transform.position.x, 3.0f), 0.025f);
+                    new Vector3(musicSheetPos.transform.position.x, Camera.main.transform.position.y + 3.0f), 0.025f);
 
 
             for (int i = 0; i < gameObject.transform.childCount; i++)
@@ -120,7 +120,7 @@ public class HamonicsDisplay : MonoBehaviour
             noteSpawnerScript.NoteSpawnReset();
 
             musicSheetPos.transform.position = Vector3.MoveTowards(musicSheetPos.transform.position,
-                   new Vector3(musicSheetPos.transform.position.x, 7.0f), 0.025f);
+                   new Vector3(musicSheetPos.transform.position.x, Camera.main.transform.position.y + 7.0f), 0.025f);
             if (musicSheetPos.transform.position.y >= 7.0f) 
             {
                 musicSheetPos.SetActive(false);
